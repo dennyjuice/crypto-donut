a7cb12afcd99a25d88fa4380f4c1bde2c0bd3d5a
onetakt <dennyjuice@gmail.com>
2022-03-21 14:05:56 +0700

    add sen donation and test

--- programs/crypto-donut/src/lib.rs
+++ programs/crypto-donut/src/lib.rs
@@ -5,11 +5,70 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod crypto_donut {
     use super::*;
+    use anchor_lang::solana_program::entrypoint::ProgramResult;
+    use anchor_lang::solana_program::program::invoke;
+    use anchor_lang::solana_program::system_instruction::transfer;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.owner = ctx.accounts.user.to_account_info().key();
         Ok(())
     }
+
+    pub fn send_donation(ctx: Context<Donation>, amount: u64) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.donators.push(ctx.accounts.user.key());
+
+        let instruction = transfer(ctx.accounts.user.key, &base_account.key(), amount);
+
+        invoke(
+            &instruction,
+            &[
+                ctx.accounts.user.to_account_info(),
+                base_account.to_account_info(),
+            ],
+        )
+        .unwrap();
+
+        Ok(())
+    }
+
+    // pub fn withdraw(ctx: Context<Donation>) -> ProgramResult {
+    //     let instruction = transfer(
+    //         &ctx.accounts.base_account.key(),
+    //         &ctx.accounts.user.owner,
+    //         ctx.accounts.base_account.to_account_info().lamports(),
+    //     );
+    //
+    //     invoke(
+    //         &instruction,
+    //         &[
+    //             ctx.accounts.base_account.to_account_info(),
+    //             ctx.accounts.user.to_account_info(),
+    //         ],
+    //     )
+    // }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 64 + 1024)]
+    pub base_account: Account<'info, BaseAccount>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub donators: Vec<Pubkey>,
+    pub owner: Pubkey,
+}
+
+#[derive(Accounts)]
+pub struct Donation<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+    pub user: Signer<'info>,
+}
--- tests/crypto-donut.ts
+++ tests/crypto-donut.ts
@@ -1,16 +1,56 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { CryptoDonut } from "../target/types/crypto_donut";
+import { expect } from "chai";
 
 describe("crypto-donut", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.CryptoDonut as Program<CryptoDonut>;
+  let baseAccount: anchor.web3.Keypair;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
-  });
+  it("Should create account", async () => {
+      baseAccount = anchor.web3.Keypair.generate();
+      console.log("p", baseAccount.publicKey);
+
+      const tx = await program.rpc.initialize({
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        },
+        signers: [baseAccount],
+      });
+
+      const account = await program.account.baseAccount.fetch(
+        baseAccount.publicKey
+      );
+
+      expect(account.owner).to.deep.equal(provider.wallet.publicKey);
+    });
+
+  it("Should send donation", async () => {
+      const donator = anchor.web3.Keypair.generate();
+
+      console.log(donator.publicKey, baseAccount.publicKey);
+
+      await provider.connection.confirmTransaction(
+          await provider.connection.requestAirdrop(donator.publicKey, 10000000000),
+          "confirmed"
+        );
+
+        const tx = await program.rpc.sendDonation(new anchor.BN(100), {
+          accounts: {
+              baseAccount: baseAccount.publicKey,
+              user: donator.publicKey,
+          },
+          signers: [donator],
+        });
+
+        const balance = await program.account.baseAccount.getAccountInfo(donator.publicKey);
+      console.log(balance);
+        expect(balance.lamports.toString()).equal("100");
+      });
 });
