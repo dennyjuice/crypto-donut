4411b72387bfd3414996a15a4fd1f4ece53872b5
onetakt <dennyjuice@gmail.com>
2022-03-24 04:12:46 +0700

    fix confirm options not found type, add readme, change to devnet

---
+++ README.md
@@ -0,0 +1,9 @@
+##Working dApp on devnet
+[HERE]()
+
+##Screenshots
+
+||||
+| --- | --- | --- |
+| ![](screenshots/1.png?raw=true) | ![](screenshots/2.png?raw=true)  | ![](screenshots/3.png?raw=true) |
+
--- app/src/types/index.ts
+++ app/src/types/index.ts
@@ -1,3 +1,20 @@
 export type { CryptoDonut } from "./crypto-donut";
 
 export type DonatorsType = { user: string; amount: string }[];
+
+export type ConfirmOptions = {
+    skipPreflight?: boolean;
+    commitment?: Commitment;
+    preflightCommitment?: Commitment;
+    maxRetries?: number;
+};
+
+export type Commitment =
+    | 'processed'
+    | 'confirmed'
+    | 'finalized'
+    | 'recent'
+    | 'single'
+    | 'singleGossip'
+    | 'root'
+    | 'max';
